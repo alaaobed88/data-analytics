01effd213360e0d364992cbb0a0bebc527767efb
Alaa Obeid <alaaobed88@gmail.com>
2023-07-31 02:31:10 +0300

    finished Package component with responsive styling

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,11 +1,65 @@
-import React from 'react'
-import Single from '../Assets/single.png';
-import Double from '../Assets/double.png';
-import Triple from '../Assets/triple.png';
+import React from "react";
+import Single from "../Assets/single.png";
+import Double from "../Assets/double.png";
+import Triple from "../Assets/triple.png";
 const Packages = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Single}
+            alt="single-package"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
+          <p className="text-center text-4xl font-bold">$100</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">250gb Storage</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
+          </div>
+          <button className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] mx-auto my-6 px-6 py-3">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full bg-gray-100 shadow-xl flex flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-transparent"
+            src={Double}
+            alt="single-package"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Partnership</h2>
+          <p className="text-center text-4xl font-bold">$199</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">1 TB Storage</p>
+            <p className="py-2 border-b mx-8">3 Granted Users</p>
+            <p className="py-2 border-b mx-8">Send up to 10 GB</p>
+          </div>
+          <button className="bg-[#000300] text-[#00df9a] rounded-md font-medium w-[200px] mx-auto my-6 px-6 py-3">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt="single-package"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Group Account</h2>
+          <p className="text-center text-4xl font-bold">$299</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">5 TB Storage</p>
+            <p className="py-2 border-b mx-8">10 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 20 GB</p>
+          </div>
+          <button className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] mx-auto my-6 px-6 py-3">
+            Start Trial
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Packages;
